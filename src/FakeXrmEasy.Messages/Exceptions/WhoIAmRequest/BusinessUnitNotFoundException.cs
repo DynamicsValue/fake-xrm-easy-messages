ccb3f31c2082b9cb2f7a46324982c605f0f6cb3f
Jordi <jordi.montana@gmail.com>
2023-09-23 13:53:28 +0200

    Add missing XML comments to the new exception

--- src/FakeXrmEasy.Messages/Exceptions/WhoIAmRequest/BusinessUnitNotFoundException.cs
+++ src/FakeXrmEasy.Messages/Exceptions/WhoIAmRequest/BusinessUnitNotFoundException.cs
@@ -2,12 +2,23 @@ using System;
 
 namespace FakeXrmEasy.FakeMessageExecutors.Exceptions.WhoIAmRequest
 {
+    /// <summary>
+    /// Throws an exception when a business unit id was set in the CallerProperties that wasn't previously initialised
+    /// </summary>
     public class BusinessUnitNotFoundException: Exception
     {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public BusinessUnitNotFoundException()
         {
             
         }
+        
+        /// <summary>
+        /// Exception constructor with the offending business unit id
+        /// </summary>
+        /// <param name="businessUnitId">The business unit id that was not initialised</param>
         public BusinessUnitNotFoundException(Guid businessUnitId) 
             : base($"The current user 'callerId' has a business unit property id value '{businessUnitId.ToString()}' that was not initialized in the context.")
         {
