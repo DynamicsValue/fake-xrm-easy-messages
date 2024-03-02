a41d1643c8789592c7f87c74d953787ced6f721d
Jordi <jordi.montana@gmail.com>
2024-03-02 18:14:37 +0100

    Add missing XML comments

--- src/FakeXrmEasy.Messages/FakeMessageExecutors/RetrieveCurrentOrganizationRequestExecutor.cs
+++ src/FakeXrmEasy.Messages/FakeMessageExecutors/RetrieveCurrentOrganizationRequestExecutor.cs
@@ -15,11 +15,23 @@ namespace FakeXrmEasy.FakeMessageExecutors
     /// </summary>
     public class RetrieveCurrentOrganizationRequestExecutor: IFakeMessageExecutor
     {
+        /// <summary>
+        /// Determines if the given request can be executed by this executor
+        /// </summary>
+        /// <param name="request">The OrganizationRequest that is currently executing</param>
+        /// <returns></returns>
         public bool CanExecute(OrganizationRequest request)
         {
             return request is RetrieveCurrentOrganizationRequest;
         }
 
+        /// <summary>
+        /// Implements the execution of the current request with this executor against a particular XrmFakedContext
+        /// </summary>
+        /// <param name="request">The current request that is being executed</param>
+        /// <param name="ctx">The instance of an XrmFakedContext that the request will be executed against</param>
+        /// <returns>RetrieveCurrentOrganizationResponse</returns>
+        /// <exception cref="Exception"></exception>
         public OrganizationResponse Execute(OrganizationRequest request, IXrmFakedContext ctx)
         {
             OrganizationDetail orgDetail = null; 
@@ -47,6 +59,10 @@ namespace FakeXrmEasy.FakeMessageExecutors
             };
         }
 
+        /// <summary>
+        /// Returns the type of the concrete OrganizationRequest that this executor implements
+        /// </summary>
+        /// <returns></returns>
         public Type GetResponsibleRequestType()
         {
             return typeof(RetrieveCurrentOrganizationRequest);
