e7c8aab0731f4e89664f973c998eaa9846a161bf
Jordi <jordi.montana@gmail.com>
2024-03-02 17:09:33 +0100

    Resolve missing XML comments

--- src/FakeXrmEasy.Messages/Exceptions/NavigateToNextEntityOrganizationRequest/CurrentEntityNotFoundException.cs
+++ src/FakeXrmEasy.Messages/Exceptions/NavigateToNextEntityOrganizationRequest/CurrentEntityNotFoundException.cs
@@ -2,8 +2,16 @@ using System;
 
 namespace FakeXrmEasy.Messages.Exceptions.NavigateToNextEntityOrganizationRequest
 {
+    /// <summary>
+    /// Exception raised when the entity record of the current stage associated to a given business process does not exist
+    /// </summary>
     public class CurrentEntityNotFoundException: Exception
     {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="logicalName"></param>
+        /// <param name="id"></param>
         public CurrentEntityNotFoundException(string logicalName, Guid id): 
             base($"There is no current entity record with logical name '{logicalName}' and Id '{id}'")
         {
