3adc0b80876f28a19e3aafcaaeb9249b0dc06660
Jordi <jordi.montana@gmail.com>
2024-08-23 22:29:37 +0200

    Implements the DeleteFileRequest message DynamicsValue/fake-xrm-easy#157

--- src/FakeXrmEasy.Messages/FakeMessageExecutors/DeleteFileRequestExecutor.cs
+++ src/FakeXrmEasy.Messages/FakeMessageExecutors/DeleteFileRequestExecutor.cs
@@ -2,30 +2,29 @@
 using System;
 using FakeXrmEasy.Abstractions;
 using FakeXrmEasy.Abstractions.FakeMessageExecutors;
-using FakeXrmEasy.Core.FileStorage.Download;
 using Microsoft.Crm.Sdk.Messages;
 using Microsoft.Xrm.Sdk;
 
 namespace FakeXrmEasy.FakeMessageExecutors
 {
     /// <summary>
-    /// Implements the DownloadBlockRequest message https://learn.microsoft.com/en-us/dotnet/api/microsoft.crm.sdk.messages.downloadblockrequest?view=dataverse-sdk-latest
+    /// Implements the DeleteFileRequest message https://learn.microsoft.com/en-us/dotnet/api/microsoft.crm.sdk.messages.deletefilerequest?view=dataverse-sdk-latest
     /// </summary>
-    public class DownloadBlockRequestExecutor: IFakeMessageExecutor
+    public class DeleteFileRequestExecutor: IFakeMessageExecutor
     {
         /// <summary>
-        /// Returns true if the request is a DownloadBlockRequest
+        /// Returns true if the request is a DeleteFileRequest message
         /// </summary>
         /// <param name="request"></param>
         /// <returns></returns>
         /// <exception cref="NotImplementedException"></exception>
         public bool CanExecute(OrganizationRequest request)
         {
-            return request is DownloadBlockRequest;
+            return request is DeleteFileRequest;
         }
 
         /// <summary>
-        /// Executes a fake implementation of DownloadBlockRequest
+        /// Implements a fake DeleteFileRequest message
         /// </summary>
         /// <param name="request"></param>
         /// <param name="ctx"></param>
@@ -33,33 +32,22 @@ namespace FakeXrmEasy.FakeMessageExecutors
         /// <exception cref="NotImplementedException"></exception>
         public OrganizationResponse Execute(OrganizationRequest request, IXrmFakedContext ctx)
         {
-            var req = request as DownloadBlockRequest;
+            var req = request as DeleteFileRequest;
             
             var fileDb = (ctx as XrmFakedContext).FileDb;
+            fileDb.DeleteFile(req.FileId.ToString());
 
-            var data = fileDb.DownloadFileBlock(new DownloadBlockProperties()
-            {
-                FileDownloadSessionId = req.FileContinuationToken,
-                Offset = req.Offset,
-                BlockLength = req.BlockLength
-            });
-            
-            return new DownloadBlockResponse()
-            {
-                Results = new ParameterCollection()
-                {
-                    { "Data", data }
-                }
-            };
+            return new DeleteFileResponse();
         }
 
         /// <summary>
-        /// Returns the DownloadBlockRequest type
+        /// Returns the type of DeleteFileRequest
         /// </summary>
         /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         public Type GetResponsibleRequestType()
         {
-            return typeof(DownloadBlockRequest);
+            return typeof(DeleteFileRequest);
         }
     }
 }
